1828637e979915dc7fc12ce9a432f5bb868d22cd
Adam Cole <acole@slack-corp.com>
2021-07-29 03:28:36 -0400

    prep for heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import path, { join } from "path";
 
 import { getData, initialize, addDataEntry } from "./data/data-manager.js";
 
@@ -16,6 +17,7 @@ var upload = multer({ storage });
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(
@@ -37,6 +39,14 @@ app.get("/get_initial_data", (req, res) => {
   const data = getData();
   res.send({ data });
 });
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.post("/add_data_entry", (req, res) => {
   const dataEntry = req.body;
