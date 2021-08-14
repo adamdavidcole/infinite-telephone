162e19aadfd3e3ad0b01ff7ffd078352afbbcc65
Adam Cole <acole@slack-corp.com>
2021-08-14 03:51:51 -0400

    growing wire animation

--- client/src/p5-scripts/wire.js
+++ client/src/p5-scripts/wire.js
@@ -5,6 +5,14 @@ import Spring from "./spring";
 const PARTCILES_PER_STRING = 10;
 const k = 1;
 
+const ANIMATION_STEPS = {
+  BEFORE_ANIMATION: "BEFORE_ANIMATION",
+  ANIMATING: "ANIMATING",
+  AFTER_ANIMATION: "AFTER_ANIMATION",
+};
+
+const { BEFORE_ANIMATION, ANIMATING, AFTER_ANIMATION } = ANIMATION_STEPS;
+
 export default class Wire {
   constructor({ start, end, word, weight, p }) {
     this.start = start;
@@ -13,6 +21,10 @@ export default class Wire {
 
     this.word = word;
     this.weight = weight;
+
+    this.animationDuration = 5000;
+    this.animationStatus = ANIMATING;
+    this.animationStartTime = Date.now();
 
     this.particles = [];
     this.springs = [];
@@ -44,6 +56,11 @@ export default class Wire {
     this.tail.position = end;
   }
 
+  getAnimationProgress(p) {
+    const timeElapsed = Date.now() - this.animationStartTime;
+    return p.min(timeElapsed / this.animationDuration, 1);
+  }
+
   update() {
     this.particles.forEach((particle) => {
       particle.update();
@@ -58,24 +75,42 @@ export default class Wire {
     const r = p.pow(this.word.charCodeAt(0), 2) % 255;
     const g = 60;
     const b = p.pow(this.word.charCodeAt(1), 1) % 255;
-    // p.fill(r, 50, b);
-    p.strokeWeight(this.weight);
 
+    p.strokeWeight(this.weight);
     const stroke = p.map(this.weight, 1, 5, 100, 200);
     p.stroke(r, g, b, stroke);
-
     p.noFill();
-    if (this.word === "family")
-      console.log("r", r, "g", g, "b", b, "a", stroke);
 
-    // p.line(0, 0, p.random(500), p.random(500));
+    const animationProgress = this.getAnimationProgress(p);
 
     p.beginShape();
-    p.curveVertex(this.head.position.x, this.head.position.y);
+    let startPosition = this.head.position;
+
+    const scaleY = p.map(animationProgress, 0, 1, -0.5, 1);
+
+    p.push();
+    p.translate(startPosition.x, startPosition.y);
+    p.scale(animationProgress, scaleY);
+    p.curveVertex(0, 0);
+
     this.particles.forEach((particle) => {
-      p.curveVertex(particle.position.x, particle.position.y);
+      const position = particle.position.copy().sub(startPosition);
+      p.curveVertex(position.x, position.y);
     });
-    p.curveVertex(this.tail.position.x, this.tail.position.y);
+
+    let tailPosition = this.particles[this.particles.length - 1].position
+      .copy()
+      .sub(startPosition);
+    p.curveVertex(tailPosition.x, tailPosition.y);
     p.endShape();
+    p.pop();
+
+    // p.beginShape();
+    // p.curveVertex(this.head.position.x, this.head.position.y);
+    // this.particles.forEach((particle) => {
+    //   p.curveVertex(particle.position.x, particle.position.y);
+    // });
+    // p.curveVertex(this.tail.position.x, this.tail.position.y);
+    // p.endShape();
   }
 }
